5bdd7af50ed0e4e0ff34ffc9092571579807f489
Steven Wu <steven.j.wu@gmail.com>
2017-01-16 16:40:30 +1030

    Finish React Router chapter.

---
+++ app/components/Home.js
@@ -0,0 +1,11 @@
+var React = require('React');
+
+var Home = React.createClass({
+  render: function() {
+    return (
+      <div>Hello from Home!</div>
+    )
+  }
+});
+
+module.exports = Home;
--- app/index.js
+++ app/index.js
@@ -1,51 +1,8 @@
-var USER_DATA = {
-  name: 'Steven Wu',
-  username: 'shadeven'
-};
-
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var Link = React.createClass({
-  render: function() {
-    return (
-      <span>{this.props.children}</span>
-    );
-  }
-});
-
-var ProfileLink = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <Link href={'https://github.com/' + this.props.username}>
-          {this.props.username}
-        </Link>
-      </div>
-    )
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function() {
-    return (
-      <div>{this.props.name}</div>
-    )
-  }
-});
-
-var Avatar = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username} />
-      </div>
-    )
-  }
-});
+var routes = require('./config/routes');
 
 ReactDOM.render(
-  <Avatar user={USER_DATA}/>,
+  routes,
   document.getElementById('app')
 );
